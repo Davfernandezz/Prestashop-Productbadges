9ece4dd51d43886b74bc8af9c97bc2f4f8230054
David Fernadez <daffevafernandez@gmail.com>
2026-05-16 18:55:46 +0200

    feat: Prepare module translations

---
+++ modules/productbadges/translations/en.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * English translations for the productbadges module.
+ *
+ * Note: PrestaShop generates the real translation keys from
+ * International > Translations. This file prepares the module
+ * translation structure.
+ */
+
+global $_MODULE;
+$_MODULE = [];
---
+++ modules/productbadges/translations/es.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Traducciones al español para el módulo productbadges.
+ *
+ * Nota: PrestaShop genera las claves reales de traducción desde
+ * Internacional > Traducciones. Este archivo deja preparada la
+ * estructura de traducciones del módulo.
+ */
+
+global $_MODULE;
+$_MODULE = [];
